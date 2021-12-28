92e35020c702e577ebd05824224d11606e828ec8
jose.sampedro <jose.sampedro@denox.es>
2021-12-28 20:27:03 +0100

    Cambio de version

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.10
 
-RUN apk add --update --no-cache openssh bash
+RUN apk add --update --no-cache openssh bash bind-tools
 
 ADD entrypoint.sh /entrypoint.sh
 ADD scripts /scripts
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
 
-echo $@
+# Variables
+FILE_SCRIPT="${1}"
+SSH_HOST="${2}"
+SSH_PORT="${3}"
+SSH_USER="${4}"
+SSH_KEY="${5}"
 
-for ARGUMENT in "$@"
-do
-	KEY=$(echo $ARGUMENT | cut -f1 -d=)
-    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+# Creamos directorios esenciales para SSH
+mkdir /root/.ssh
+touch /root/.ssh/id_rsa
+touch /root/.ssh/known_hosts
+touch /root/.ssh/config
 
-    case "$KEY" in
-		variable1) variable1=${VALUE} ;;
-		variable2) variable2=${VALUE} ;;     
-        *)   
-    esac    
-done
+# Contenido de id_rsa
+echo "$SSH_KEY" >> /root/.ssh/id_rsa
 
-echo "variable1 = $variable1"
-echo "variable2 = $variable2"
+# Permisos esenciales para SSH
+chmod 700 /root/.ssh/
+chmod 600 /root/.ssh/*
+chmod u+w /root/.ssh/known_hosts
+chown -R root:root /root/.ssh/
+
+# Obtenemos la IP del host
+HOST_IP=$(dig +short $SSH_HOST)
+
+# Añadimos a servidores conocidos
+ssh-keyscan -p $SSH_PORT -t rsa,dsa $HOST_IP >> ~/.ssh/known_hosts
+
+# Ejecutamos
+ssh -p$SSH_PORT -i ~/.ssh/id_rsa $SSH_USER@$HOST_IP "bash -s" -- < /scripts/$FILE_SCRIPT.sh "${@:6}"
+
+# Salimos
+exit $?

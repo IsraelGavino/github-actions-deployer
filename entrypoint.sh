0550aa5604f498c71e262fdad68fcebc76ffb48b
jose-sampedro <95685308+jose-sampedro@users.noreply.github.com>
2021-12-30 20:51:35 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,21 +6,23 @@ SSH_HOST="${2}"
 SSH_PORT="${3}"
 SSH_USER="${4}"
 SSH_KEY="${5}"
+USER=$(id -u -n)
+GROUP=$(id -g -n)
 
 # Creamos directorios esenciales para SSH
-mkdir /root/.ssh
-touch /root/.ssh/id_rsa
-touch /root/.ssh/known_hosts
-touch /root/.ssh/config
+mkdir ~/.ssh
+touch ~/.ssh/id_rsa
+touch ~/.ssh/known_hosts
+touch ~/.ssh/config
 
 # Contenido de id_rsa
-echo "$SSH_KEY" >> /root/.ssh/id_rsa
+echo "$SSH_KEY" >> ~/.ssh/id_rsa
 
 # Permisos esenciales para SSH
-chmod 700 /root/.ssh/
-chmod 600 /root/.ssh/*
-chmod u+w /root/.ssh/known_hosts
-chown -R root:root /root/.ssh/
+chmod 700 ~/.ssh/
+chmod 600 ~/.ssh/*
+chmod u+w ~/.ssh/known_hosts
+chown -R $USER:$GROUP ~/.ssh/
 
 # Obtenemos la IP del host
 HOST_IP=$(dig +short $SSH_HOST)

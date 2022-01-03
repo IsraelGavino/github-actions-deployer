99fa13d81e3a0a2d4106768682e5cb3518bab66a
jose-sampedro <95685308+jose-sampedro@users.noreply.github.com>
2022-01-03 13:28:37 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,12 +34,8 @@ ssh-keyscan -p $SSH_PORT -t rsa,dsa $HOST_IP >> ~/.ssh/known_hosts
 # log
 # lftp sftp://u99555015-6m4hDQd5:^6m4hDQd5%iD@access811083711.webspace-data.io -e "set sftp:auto-confirm yes; put ~/.ssh/id_rsa; bye"
 
-ls -la /scripts
-ls -la ~/.ssh
-
 # Ejecutamos
-pepe='ssh -p'$SSH_PORT' -i ~/.ssh/id_rsa '$SSH_USER'@'$HOST_IP' "bash -s" -- < /scripts/'$FILE_SCRIPT'.sh '"${@:6}"')'
-echo $pepe
+ssh -p$SSH_PORT -i ~/.ssh/id_rsa $SSH_USER@$HOST_IP "bash -s" -- < /scripts/$FILE_SCRIPT.sh "${@:6}"
 
 # Salimos
 exit $?

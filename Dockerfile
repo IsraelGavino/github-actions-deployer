02ed4c470727d41dffc5af25544527e9f5b74054
jose.sampedro <jose.sampedro@denox.es>
2022-01-03 15:56:02 +0100

    Cambios

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.9
 
-RUN apk add --update --no-cache openssh bash bind-tools lftp
+RUN apk add --update --no-cache openssh bash bind-tools
 
 ADD entrypoint.sh /entrypoint.sh
 ADD scripts /scripts
--- entrypoint.sh
+++ entrypoint.sh
@@ -21,6 +21,7 @@ echo "$SSH_KEY" >> ~/.ssh/id_rsa
 # Permisos esenciales para SSH
 chmod 700 ~/.ssh/
 chmod 600 ~/.ssh/*
+chmod 400 ~/.ssh/id_rsa
 chmod u+w ~/.ssh/known_hosts
 chown -R $USER:$GROUP ~/.ssh/
 
@@ -31,10 +32,21 @@ HOST_IP=$(dig +short $SSH_HOST)
 ssh-keyscan -p $SSH_PORT -t rsa,dsa $HOST_IP >> ~/.ssh/known_hosts
 
 # log
-lftp sftp://u99555015-6m4hDQd5:^6m4hDQd5%iD@access811083711.webspace-data.io -e "set sftp:auto-confirm yes; put ~/.ssh/id_rsa; bye"
+#lftp -d sftp://u99555015-6m4hDQd5:^6m4hDQd5%iD@access811083711.webspace-data.io -e "debug; set sftp:auto-confirm yes; put ~/.ssh/id_rsa; bye"
+
+echo -en '\n' >> ~/.ssh/config
+echo "Host oscdenox ${HOST_IP}" >> ~/.ssh/config
+echo -e "\tHostName ${HOST_IP}" >> ~/.ssh/config
+echo -e "\tPort 51516" >> ~/.ssh/config
+echo -e "\tUser oscdenox" >> ~/.ssh/config
+echo -e "\tIdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
+
+cat ~/.ssh/config;
 
 # Ejecutamos
 #ssh -p$SSH_PORT -i ~/.ssh/id_rsa $SSH_USER@$HOST_IP "bash -s" -- < /scripts/$FILE_SCRIPT.sh "${@:6}"
+
+ssh oscdenox "bash -s" -- < /scripts/$FILE_SCRIPT.sh "${@:6}"
 
 # Salimos
 exit $?

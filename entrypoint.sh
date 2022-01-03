7813e92bf3b5d5a12072e9def1e7883733b4e54e
jose-sampedro <95685308+jose-sampedro@users.noreply.github.com>
2022-01-03 19:47:22 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,20 +33,9 @@ ssh-keyscan -p $SSH_PORT -t rsa,dsa $HOST_IP >> /etc/ssh/ssh_known_hosts
 # log
 #lftp -d sftp://u99555015-6m4hDQd5:^6m4hDQd5%iD@access811083711.webspace-data.io -e "debug; set sftp:auto-confirm yes; put ~/.ssh/id_rsa; bye"
 
-echo -en '\n' >> ~/.ssh/config
-echo "Host oscdenox ${HOST_IP}" >> ~/.ssh/config
-echo -e "\tHostName ${HOST_IP}" >> ~/.ssh/config
-echo -e "\tPort 51516" >> ~/.ssh/config
-echo -e "\tUser oscdenox" >> ~/.ssh/config
-echo -e "\tIdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
-
-echo -en '\n\n' >> /etc/ssh/ssh_config
-echo "Include ${HOME}/.ssh/config" >> /etc/ssh/ssh_config
-
-# lftp sftp://u99555015-6m4hDQd5:^6m4hDQd5%iD@access811083711.webspace-data.io -e "set sftp:auto-confirm yes; put ~/.ssh/id_rsa; bye"
 
 # Ejecutamos
-ssh oscdenox "bash -s" -- < /scripts/$FILE_SCRIPT.sh "${@:6}"
+ssh -p$SSH_PORT -i ~/.ssh/id_rsa $SSH_USER@$HOST_IP "bash -s" -- < /scripts/$FILE_SCRIPT.sh "${@:6}"
 
 # Salimos
 exit $?

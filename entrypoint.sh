d0519c7c23d98627a63b697916b72ef3717c3ad7
jose-sampedro <95685308+jose-sampedro@users.noreply.github.com>
2022-02-10 11:41:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,44 +30,47 @@ SSH_HOST_IP=$(dig +short ${SSH_HOST})
 # Librerias
 . "/scripts/base.sh"
 
-echo "🕸️ Configuramos SSH"
-ssh_config "$SSH_KEY" $USER $GROUP $SSH_HOST $SSH_PORT $SSH_HOST_IP
+echo $CLEAN_UP
 
-echo "🎻 Obtenemos version composer"
-VERSION=`api_github_composer_version $GITHUB_TOKEN $GITHUB_REPOSITORY $BRANCH_NAME`
 
-# Directorio release
-PATH_RELEASE=$PATH_PUBLIC/deploy/releases/$VERSION
+# echo "🕸️ Configuramos SSH"
+# ssh_config "$SSH_KEY" $USER $GROUP $SSH_HOST $SSH_PORT $SSH_HOST_IP
 
-echo "⚙️ Ejecutamos Setup"
-ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "setup" $PATH_PUBLIC $PATH_RELEASE
+# echo "🎻 Obtenemos version composer"
+# VERSION=`api_github_composer_version $GITHUB_TOKEN $GITHUB_REPOSITORY $BRANCH_NAME`
 
-echo "🔒 Ejecutamos Lock"
-ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "lock" $PATH_PUBLIC $USERNAME
+# # Directorio release
+# PATH_RELEASE=$PATH_PUBLIC/deploy/releases/$VERSION
 
-echo "📁 Ejecutamos Release"
-ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "release" $PATH_PUBLIC $VERSION $USERNAME
+# echo "⚙️ Ejecutamos Setup"
+# ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "setup" $PATH_PUBLIC $PATH_RELEASE
 
-echo "⏬ Ejecutamos Download"
-DOWNLOAD_FILENAME=$(api_github_download_release $GITHUB_TOKEN $GITHUB_REPOSITORY $BRANCH_NAME $VERSION)
+# echo "🔒 Ejecutamos Lock"
+# ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "lock" $PATH_PUBLIC $USERNAME
 
-echo "⏫ Ejecutamos Upload"
-scp_upload_file $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP $DOWNLOAD_FILENAME $PATH_RELEASE
+# echo "📁 Ejecutamos Release"
+# ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "release" $PATH_PUBLIC $VERSION $USERNAME
 
-echo "🗜️ Ejecutamos Unpack"
-ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "unpack" $PATH_RELEASE $DOWNLOAD_FILENAME
+# echo "⏬ Ejecutamos Download"
+# DOWNLOAD_FILENAME=$(api_github_download_release $GITHUB_TOKEN $GITHUB_REPOSITORY $BRANCH_NAME $VERSION)
 
-echo "🤝 Ejecutamos Shared"
-ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "shared" $PATH_PUBLIC $PATH_RELEASE "'"$DIRS_SHARE"'" "'"$FILES_SHARE"'"
+# echo "⏫ Ejecutamos Upload"
+# scp_upload_file $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP $DOWNLOAD_FILENAME $PATH_RELEASE
 
-echo "🎻 Ejecutamos Composer"
-ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "composer" $PATH_RELEASE
+# echo "🗜️ Ejecutamos Unpack"
+# ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "unpack" $PATH_RELEASE $DOWNLOAD_FILENAME
 
-echo "⚓ Ejecutamos Symlink"
-ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "symlink" $PATH_PUBLIC $PATH_RELEASE
+# echo "🤝 Ejecutamos Shared"
+# ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "shared" $PATH_PUBLIC $PATH_RELEASE "'"$DIRS_SHARE"'" "'"$FILES_SHARE"'"
 
-echo "🧽 Ejecutamos Cleanup"
-ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "cleanup" $PATH_PUBLIC $KEEP_RELEASES
+# echo "🎻 Ejecutamos Composer"
+# ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "composer" $PATH_RELEASE
 
-echo "🔓 Ejecutamos Unlock"
-ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "unlock" $PATH_PUBLIC
+# echo "⚓ Ejecutamos Symlink"
+# ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "symlink" $PATH_PUBLIC $PATH_RELEASE
+
+# echo "🧽 Ejecutamos Cleanup"
+# ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "cleanup" $PATH_PUBLIC $KEEP_RELEASES
+
+# echo "🔓 Ejecutamos Unlock"
+# ssh_execute_remote $SSH_HOST $SSH_PORT $SSH_USER $SSH_HOST_IP "unlock" $PATH_PUBLIC
